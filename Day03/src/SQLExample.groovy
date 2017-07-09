78bbdbc61aceedd9b7c4cf42719575b6ef6f8d8d
prabhu-durasoft <prabhu@durasoftindia.com>
2017-07-09 17:29:09 +0530

    Day03

--- Day03/src/SQLExample.groovy
+++ Day03/src/SQLExample.groovy
@@ -1,6 +1,9 @@
 import groovy.sql.Sql;
 
-@Grab("mysql:mysql-connector-java:5.1.25")
+@Grapes([
+	@Grab("mysql:mysql-connector-java:5.1.25"),
+	@Grab("log4j:log4j:1.2.17")
+])
 @GrabConfig(systemClassLoader = true)
 
 sql = Sql.newInstance("jdbc:mysql://localhost/training", 
